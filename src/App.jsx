585eaffe47c6ae8dc381331ea8ae8b4cbc496600
krish bavarva <krishbavarva@krishs-iMac-Pro.local>
2024-07-23 16:37:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { incNumber, decNumber } from './action/Index'
+
+function App() {
+  const addState= useSelector((state) => state.changeTheNumber)
+  const removeState= useSelector((state) => state.changeNumber)
+  const total = addState + removeState
+  const dispatch = useDispatch()
+
+  return (
+    <>
+    <h1>welocome to redux</h1>
+    <div className="display">
+      <button onClick={() => dispatch(decNumber())} title='Decrement' >-</button>
+      <h1>{total}</h1>
+      <button onClick={() => dispatch(incNumber())} title='Increment' >+</button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/reducer/UpDown.jsx
@@ -0,0 +1,10 @@
+
+const changeTheNumber = (state = 0 , action) =>{
+    switch(action.type){
+        case "INCREMENT" : return state + 1;
+        default: return state
+    }
+
+}
+
+export default changeTheNumber;
